7f9bf0550b97172378b7befd75f2f319e51876bb
mrirfanto <mrirfanto.work@gmail.com>
2020-10-06 09:41:25 +0700

    header, images asset, product list data

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Welcome to gshop!</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to gshop!</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
