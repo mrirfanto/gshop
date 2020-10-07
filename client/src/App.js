1479c715e13fbec8d70479a4c54c4cc933211041
mrirfanto <mrirfanto.work@gmail.com>
2020-10-07 10:42:59 +0700

    home screen and product detail

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
 
 const App = () => {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to gshop!</h1>
+          <Route path="/" component={Home} exact />
+          <Route path="/product/:id" component={ProductDetail} />
         </Container>
       </main>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 };
 
