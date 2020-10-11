d133a0f7d6dda27593d072f7f098f504b7414ab4
mrirfanto <mrirfanto.work@gmail.com>
2020-10-11 17:30:31 +0700

    serving and fetching data from mongodb

--- server/server.js
+++ server/server.js
@@ -1,22 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
+import connectDB from './config/database.js'
+import productRoutes from './routes/productRoutes.js'
 
 const app = express()
 dotenv.config()
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('API server is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/product/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 
