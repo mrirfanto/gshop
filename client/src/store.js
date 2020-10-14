771f980a19e951afb838c749c0675180ff7497be
mrirfanto <mrirfanto.work@gmail.com>
2020-10-14 11:21:53 +0700

    implement redux to manage state

---
+++ client/src/store.js
@@ -0,0 +1,25 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetail: productDetailsReducer,
+});
+
+const middleware = [thunk];
+
+const initialState = {};
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
